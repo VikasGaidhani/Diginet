67c734a115f6fac93632c7e2bcbd2d32e5a1e598
VikasGaidhani <vikasgaidhani@gmail.com>
2024-04-22 17:03:05 +0530

    cc

--- src/Components/Contactform.jsx
+++ src/Components/Contactform.jsx
@@ -2,24 +2,45 @@ import React from 'react';
 import './Contact.css';
 
 export default function Contactform() {
-  const handleFormSubmit = () => {
-    console.log('Form submitted!');
-    
+  const handleFormSubmit = async (event) => {
+    event.preventDefault(); 
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch('', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        console.log('Form submitted successfully!');
+        form.reset(); 
+      } else {
+        console.error('Form submission failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
   };
 
-return (
-    <div className="container-fluid mt-4 contact-info"  >
+  return (
+    <div className="container-fluid mt-4 contact-info">
       <div className="row justify-content-center px-3">
-        <div className="col-lg-6 col-md-10 col-sm-6 ">
-          <div className="contact-info ">
-            <div className="section-title ">
+        <div className="col-lg-6 col-md-10 col-sm-6">
+          <div className="contact-info">
+            <div className="section-title">
               <span className="title-tag">Contact info</span>
               <h2 className="title m-2">Catch us here</h2>
             </div>
             <div className="contact-info-list row mobile_no_style">
               <div className="col-lg-4 col-md-4 col-sm-4 address">
                 <p>
-                <i class="fa-solid fa-location-dot icons"></i>
+                  <i className="fa-solid fa-location-dot icons"></i>
                   B 76, NICE Area, MIDC, Satpur <br /> Nashik, Maharashtra 422007
                 </p>
               </div>
@@ -31,75 +52,54 @@ return (
                   </a>
                 </p>
               </div>
-              <div className="col-lg-4  col-md-4 col-sm-4 address text-center">
+              <div className="col-lg-4 col-md-4 col-sm-4 address text-center">
                 <p>
-                <i class="fa-solid fa-phone icons"></i>India
+                  <i className="fa-solid fa-phone icons"></i>India
                 </p>
                 <a href="tel:+91 8329905564" className="contact_text_style">
-                  +91 8329905564
+                  +91 8007030184
                 </a>
-              </div>
-              {/* <div className="col-lg-4 col-md-4 col-sm-4 address">
-                <p>
-                <i class="fa-solid fa-phone icons"></i> UAE
-                </p>
-                <a className="contact_text_style" href="tel:+971 540 570 0729">
-                  +971 540 570 0729
-                </a>
-              </div>
-              <div className="col-lg-4 col-md-4 col-sm-4 address">
-                <p>
-                <i class="fa-solid fa-phone icons"></i> Los Angeles
-                </p>
-                <a className="contact_text_style" href="tel:(213) 685-0016">
-                  +213 685 0016
-                </a>
-              </div> */}
-            </div>
-          </div>
-          {/* Contact Form */}
-          <div className="contact-form mt-4">
-            <div className="row">
-              <div className="col-lg-6 mb-4 ">
-                <input type="text" className="form-control custom-input" placeholder="Enter Name" id="name" name="name" />
-              </div>
-              <div className="col-lg-6 mb-4">
-                <input type="email" className="form-control custom-input" placeholder="Email" id="email" name="email" />
-              </div>
-              <div className="col-lg-6 mb-4">
-                <input type="text" className="form-control custom-input" placeholder="Company name" id="companyname" name="companyname" />
-              </div>
-              <div className="col-lg-6 mb-4">
-                <input type="tel" className="form-control custom-input" placeholder="Phone" id="phone" name="phone" />
-              </div>
-              <div className="col-12 mb-4">
-                <textarea className="textarea form-control custom-input" id="help" name="help" placeholder="How Can We Help?"></textarea>
-              </div>
-              <div className="col-12 text-center">
-                <button type="button" className="btn " onClick={handleFormSubmit}>
-                  Submit
-                </button>
               </div>
             </div>
           </div>
+          
+          <form onSubmit={handleFormSubmit}>
+            <div className="contact-form mt-4">
+              <div className="row">
+                <div className="col-lg-6 mb-4">
+                  <input type="text" className="form-control custom-input" placeholder="Enter Name" id="name" name="name" />
+                </div>
+                <div className="col-lg-6 mb-4">
+                  <input type="email" className="form-control custom-input" placeholder="Email" id="email" name="email" />
+                </div>
+                <div className="col-lg-6 mb-4">
+                  <input type="text" className="form-control custom-input" placeholder="Company name" id="companyname" name="companyname" />
+                </div>
+                <div className="col-lg-6 mb-4">
+                  <input type="tel" className="form-control custom-input" placeholder="Phone" id="phone" name="phone" />
+                </div>
+                <div className="col-12 mb-4">
+                  <textarea className="textarea form-control custom-input" id="help" name="help" placeholder="How Can We Help?"></textarea>
+                </div>
+                <div className="col-12 text-center">
+                  <button type="submit" className="btn">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
         <div className="col-lg-5 col-md-10 contact-maps">
-          <iframe className='map'
+          <iframe
+            className="map"
             src="https://maps.google.com/maps?q=Spandigit%20Social,%20B76,NICE%20Area,%20MIDC,%20Satpur%20Colony,%20Nashik,%20Maharashtra%20422007&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
             allowFullScreen=""
             aria-hidden="false"
             tabIndex="0"
           ></iframe>
-
-
-    
         </div>
       </div>
     </div>
-
-
-
-
-
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,52 +1,4 @@
-// import React from 'react';
-// // import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import './Navbar.css'
-// import {Link} from "react-router-dom"
 
-
-// export default function Navbar() {
-//   return (
-    
-//       <nav class="navbar navbar-expand-lg bg-body-tertiary mb-0 position-Fixed ">
-//         <div class="container-fluid bg-dark border-bottom border-warning border-5 p-3 ">
-//           <a class="navbar-brand text-white fs-4" href="#">DIGINET</a>
-//           <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//             <span class="navbar-toggler-icon "></span>
-//           </button>
-//           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-//               <li class="nav-item">
-//               <Link rel="stylesheet" to="/" className='text-warning active text-decoration-none ms-5'>HOME</Link>
-//                 {/* <a class="nav-Link active text-warning" aria-current="page" href="#">HOME</a> */}
-//               </li>
-//               <li class="nav-item">
-//                 <Link rel="stylesheet" to="/about" className='text-white text-decoration-none ms-3 abc' >ABOUT US</Link>
-//                 {/* <a class="nav-Link text-white" href="#">ABOUT US</a> */}
-//               </li>
-//               <li class="nav-item">
-//               <Link rel="stylesheet" to="/services" className='text-white text-decoration-none ms-3 abc' >SERVICES</Link>
-//                 {/* <a class="nav-Link text-white" href="#">SERVICES</a> */}
-//               </li>
-//               <li class="nav-item">
-//               <Link rel="stylesheet" to="/contact" className='text-white text-decoration-none ms-3 abc' >CONTACT US</Link>
-//                 {/* <a class="nav-Link text-white" href="#">CONTACT</a> */}
-//               </li>
-//             </ul>
-
-            
-          
-//           <div class="bg-warning ">
-//               <button class="btn">
-//                 <span><i class="fas fa-phone-flip "></i> PHONE NUMBER<br />+91 1234567890</span>
-//               </button>
-//             </div> 
-//             </div> 
-//         </div>
-//       </nav>
-    
-//   );
-// }
 
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
@@ -123,7 +75,7 @@ export default function Navbar() {
               <span className='text-white'>
                 <i className="fas fa-phone-flip"></i> PHONE NUMBER
                 <br />
-                +91 1234567890
+                +91 8007030184
               </span>
             </button>
           </div>
